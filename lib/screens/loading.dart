e5edc325c47f83ee557810b35fdd8f3d52ff12ac
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2022-08-13 23:14:56 +0900

    add geolocator and permission

---
+++ lib/screens/loading.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            getLocation();
+            },
+          child: Text(
+            'get my location',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
