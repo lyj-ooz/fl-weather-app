d66223c44be70d4c6e0b6e382862b642f30eb5da
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2022-08-14 14:38:33 +0900

    add http package and fetchData method

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,38 +12,54 @@ class Loading extends StatefulWidget {
 class _LoadingState extends State<Loading> {
 
   void getLocation() async {
-
     try {
       LocationPermission permission = await Geolocator.requestPermission();
-      Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
       print(position);
-    } catch(e) {
+    } catch (e) {
       print('에러발생');
     }
   }
 
+  void fetchData() async {
+    var httpsUri = Uri(
+        scheme: 'https',
+        host: 'samples.openweathermap.org',
+        path: '/data/2.5/weather',
+        queryParameters: {
+          'q': 'London',
+          'appid': 'b1b15e88fa797225412429c1c50c122a1'
+        });
+
+    print(httpsUri);
+
+    http.Response response = await http.get(httpsUri);
+    print(response.body);
+  }
 
   @override
   void initState() {
-    getLocation();
     super.initState();
+    getLocation();
+    fetchData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            getLocation();
+          child: ElevatedButton(
+            onPressed: () {
+              getLocation();
             },
-          child: Text(
-            'get my location',
-            style: TextStyle(
-              color: Colors.white
+            child: Text(
+              'get my location',
+              style: TextStyle(
+                  color: Colors.white
+              ),
             ),
-          ),
-        )
+          )
       ),
     );
   }
